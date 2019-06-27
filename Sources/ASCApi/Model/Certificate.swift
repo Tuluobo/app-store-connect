015baf24fcd551fcac16bdd73e5a014945234f7b
Tuluobo <tuluobo@vip.qq.com>
2019-06-27 08:55:28 +0800

    [Add] add  Certificate & Profile

--- Sources/ASCApi/Model/Certificate.swift
+++ Sources/ASCApi/Model/Certificate.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+enum CertificateType: String, Model {
+    case iosDevelopment = "IOS_DEVELOPMENT"
+    case iosDistribution = "IOS_DISTRIBUTION"
+    case macDistribution = "MAC_APP_DISTRIBUTION"
+    case macDevelopment = "MAC_APP_DEVELOPMENT"
+    case macInstallerDistribution = "MAC_INSTALLER_DISTRIBUTION"
+    case developmentIDKext = "DEVELOPER_ID_KEXT"
+    case developmentIDApplication = "DEVELOPER_ID_APPLICATION"
+}
+
+public struct Certificate: Model {
+    
+    struct Attribute: Model {
+        let certificateContent: String
+        let displayName: String
+        let expirationDate: String
+        let name: String
+        let platform: BundleIdPlatform
+        let serialNumber: String
+        let certificateType: CertificateType
+    }
+    
+    let id: String
+    let type: ResourceType
+    let attributes: Attribute
+    let links: Link
+}
+
+// MARK: - Certificate List Response
+
+public struct CertificateListResponse: Model {
+    let data: [Certificate]
+    let links: Link
+    let meta: PagingInformation
+}
+
+// MARK: - Certificate Response
+
+public struct CertificateResponse: Model {
+    let data: Certificate
+    let links: Link
+}
--- Sources/ASCApi/Model/Profile.swift
+++ Sources/ASCApi/Model/Profile.swift
@@ -6,3 +6,87 @@
 //
 
 import Foundation
+
+enum ProfileType: String, Model {
+    case iosAppDevelopment  = "IOS_APP_DEVELOPMENT"
+    case iosAppStore        = "IOS_APP_STORE"
+    case iosAppAdHoc        = "IOS_APP_ADHOC"
+    case iosAppInHouse      = "IOS_APP_INHOUSE"
+    
+    case macAppDevelopment  = "MAC_APP_DEVELOPMENT"
+    case macAppStore        = "MAC_APP_STORE"
+    case macAppDirect       = "MAC_APP_DIRECT"
+    
+    case tvOSAppDevelopment = "TVOS_APP_DEVELOPMENT"
+    case tvOSAppStore       = "TVOS_APP_STORE"
+    case tvOSAppAdHoc       = "TVOS_APP_ADHOC"
+    case tvOSAppInHouse     = "TVOS_APP_INHOUSE"
+}
+
+enum ProfileState: String, Model {
+    case active = "ACTIVE"
+    case invlid = "INVALID"
+}
+
+public struct Profile: Model {
+    
+    struct Attribute: Model {
+        let uuid: String
+        let name: String
+        let platform: BundleIdPlatform
+        let profileType: ProfileType
+        let profileContent: String
+        let profileState: ProfileState
+        
+        let createdDate: String
+        let expirationDate: String
+    }
+    
+    struct Relationship: Model {
+        struct Certificates: Model {
+            let data: [ResourceModel]
+            let links: Link
+            let meta: PagingInformation
+        }
+        
+        struct Devices: Model {
+            let data: [ResourceModel]
+            let links: Link
+            let meta: PagingInformation
+        }
+        
+        struct BundleId: Model {
+            let data: ResourceModel
+            let links: Link
+        }
+        
+        let certificates: Certificates
+        let devices: Devices
+        let bundleId: BundleID
+    }
+    
+    let id: String
+    let attributes: Attribute
+    let relationships: Relationship?
+    let type: ResourceType
+    let links: Link
+}
+
+// MARK: - Profile List Response
+
+public struct ProfileListResponse: Model {
+    let data: [Profile]
+    let links: Link
+    let meta: PagingInformation
+    // Possible types: BundleId, Device, Certificate
+    // let included:
+}
+
+// MARK: - Profile Response
+
+public struct ProfileResponse: Model {
+    let data: Profile
+    let links: Link
+    // Possible types: BundleId, Device, Certificate
+    // let included:
+}
